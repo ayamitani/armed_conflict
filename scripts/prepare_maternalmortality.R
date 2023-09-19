577174f012eafe91522425d0603f516dd8c55844
Aya Mitani <ayamitani@gmail.com>
2023-09-19 15:58:55 -0400

    update script

---
+++ scripts/prepare_maternalmortality.R
@@ -0,0 +1,10 @@
+matmor <- read.csv(here("data", "original", "maternalmortality.csv"), header = TRUE)
+
+matmor |>
+  dplyr::select(Country.Name, X2000:X2019) |>
+  pivot_longer(cols = starts_with("X"),
+               names_to = "year",
+               names_prefix = "X",
+               values_to = "matmor") |>
+  mutate(year = as.numeric(year)) |>
+  arrange(Country.Name, year)
