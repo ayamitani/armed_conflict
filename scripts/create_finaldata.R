24b0516d5c73a5579547fcba28ed1c9253f4c493
Aya Mitani <ayamitani@gmail.com>
2023-09-19 16:09:01 -0400

    deleted some files

--- scripts/create_finaldata.R
+++
@@ -1,8 +0,0 @@
-#---------------------------------------------------------------------------
-# Create final data for analysis 
-# Author: Aya Mitani
-# Date: 2023-09-19
-#---------------------------------------------------------------------------
-
-source(here("scripts", "prepare_wbdata.R"))
-source(here("scripts", "prepare_disaster.R"))
