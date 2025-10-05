5973559ee167c316808c4446ea985d7184ec768f
Aya Mitani <ayamitani@gmail.com>
2025-10-05 10:19:15 -0400

    updated conflict data

--- R/prepare_final_data.R
+++ R/prepare_final_data.R
@@ -71,7 +71,7 @@ disasterdata <- disasters |>
 
 ### 3. Prepare Conflict data
 
-conflict <- read.csv(here("data//original//conflictdata.csv"), header = TRUE)
+conflict <- read.csv("data//original//conflictdata.csv", header = TRUE)
 
 conflict %>%
   # Outcome 1: Binary indicator of armed conflict (0 if <25, 1 if >= 25 battle related deaths) for each country-year
